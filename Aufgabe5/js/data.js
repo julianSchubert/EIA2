ddbc8b1c914ae3dae3324c7ee070f7c6c67a9a8e
julianSchubert <julian.schubert@hs-furtwangen.de>
2019-04-27 15:18:15 +0200

    Fertig

---
+++ Aufgabe5/js/data.js
@@ -0,0 +1,25 @@
+var Aufgabe5;
+(function (Aufgabe5) {
+    Aufgabe5.data = {
+        "Sorten 1€": [
+            { value: "Vanille", preis: 1, type: "checkbox", name: "", id: "" },
+            { value: "Schokolade", preis: 1, type: "checkbox", name: "", id: "" },
+            { value: "Nuss", preis: 1, type: "checkbox", name: "", id: "" },
+            { value: "Erdbeer", preis: 1, type: "checkbox", name: "", id: "" }
+        ],
+        "Zusaetze 0.5€": [
+            { value: "Sahne", preis: 0.5, type: "checkbox", name: "", id: "" },
+            { value: "Soße", preis: 0.5, type: "checkbox", name: "", id: "" },
+            { value: "Streusel", preis: 0.5, type: "checkbox", name: "", id: "" }
+        ],
+        "Behältnis": [
+            { value: "Becher", preis: 0.0, type: "radio", name: "be", id: "" },
+            { value: "Waffel", preis: 0.0, type: "radio", name: "be", id: "" }
+        ],
+        "Lieferart (Lieferung 2€)": [
+            { value: "Abholung", preis: 0.0, type: "radio", name: "li", id: "abholen1" },
+            { value: "Lieferung", preis: 2.0, type: "radio", name: "li", id: "abholen2" }
+        ]
+    };
+})(Aufgabe5 || (Aufgabe5 = {}));
+//# sourceMappingURL=data.js.map
---
+++ Aufgabe5/js/data.ts
@@ -0,0 +1,43 @@
+namespace Aufgabe5 {
+
+
+	export interface WasEinEisBraucht {     //heterogen
+		value: string;
+		preis: number;
+		type: string;
+		name: string;
+		id: string;
+	}
+
+	export interface Zuweisung {            //homogen
+		[key: string]: WasEinEisBraucht[];  	//key typ string
+		//key von typ Was ein Eis braucht
+	}
+
+	export let data: Zuweisung      //homogen //alles
+		= {
+		"Sorten 1€": [
+			{ value: "Vanille", preis: 1, type: "checkbox" , name: "", id : ""},
+			{ value: "Schokolade", preis: 1, type: "checkbox", name: "", id : ""},
+			{ value: "Nuss", preis: 1, type: "checkbox", name: "", id : "" },
+			{ value: "Erdbeer", preis: 1, type: "checkbox", name: "", id : "" }
+		],
+		"Zusaetze 0.5€": [
+			{ value: "Sahne", preis: 0.5, type: "checkbox", name: "", id : "" },
+			{ value: "Soße", preis: 0.5, type: "checkbox", name: "", id : ""},
+			{ value: "Streusel", preis: 0.5, type: "checkbox", name: "", id : ""}
+		],
+		"Behältnis": [
+			{ value: "Becher", preis: 0.0, type: "radio", name: "be", id : "" },
+			{ value: "Waffel", preis: 0.0, type: "radio", name: "be", id : ""}
+		],
+		"Lieferart (Lieferung 2€)": [
+			{ value: "Abholung", preis: 0.0, type: "radio", name: "li", id : "abholen1"},
+			{ value: "Lieferung", preis: 2.0, type: "radio", name: "li", id : "abholen2"}
+		]
+	};
+
+
+
+
+}
