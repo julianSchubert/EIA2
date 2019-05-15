ee4cd58dc6d4ddfca220057e7a0e8932d2ad19c3
julianSchubert <julian.schubert@hs-furtwangen.de>
2019-05-15 18:36:55 +0200

    Konzept Aufgabe 8

--- Aufgabe7/js/main.ts
+++ Aufgabe7/js/main.ts
@@ -172,7 +172,7 @@ namespace Aufgabe7 {
 
 
     export function URLerstellen(): void {
-        let serveradresse: string = "https://eia2-julian.herokuapp.com//?"; // mit localhost prrüfen
+        let serveradresse: string = "https://eia2-julian.herokuapp.com//?"; // mit localhost prüfen
         let meinobjekt: string = "";
 
         let ziel: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
